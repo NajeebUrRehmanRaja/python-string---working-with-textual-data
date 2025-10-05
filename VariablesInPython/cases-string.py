48b8148734b18991b7c46672ad82e61e7635bf97
NajeebUrRehmanRaja <najeebraja22.2@gmail.com>
2025-10-06 00:25:12 +0500

    feat: add string case manipulation examples in Python
    Demonstrate various string case operations including upper, lower, title and swapcase to help beginners understand string manipulation basics

---
+++ VariablesInPython/cases-string.py
@@ -0,0 +1,7 @@
+#  we can find the string by case
+message = "Hello Python World"
+print(message.upper())
+print(message.lower())
+print(message.title())
+#  we can find the string by case in reverse order
+print(message.swapcase())
