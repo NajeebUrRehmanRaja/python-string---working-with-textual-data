630e09b08fa247882e9b123c393a3ba2665bfb0a
NajeebUrRehmanRaja <najeebraja22.2@gmail.com>
2025-10-06 00:05:32 +0500

    docs(VariablesInPython): add example script demonstrating variable usage
    Add a Python script showing basic variable usage, reassignment, and string quoting examples

---
+++ VariablesInPython/variables-python.py
@@ -0,0 +1,20 @@
+# Here is the code which is print using the variable, we can use the variable to print the message multiple times, 
+# and we can change the value of the message as well.
+# print the simple statement
+message = "Hello World"
+print(message)
+# We can print the message multiple times
+print(message)
+print(message)
+# We can change the value of the message
+message = "Hello Python World"
+print(message)
+
+#  /////////////////////////
+
+# we can also use this approach
+message = 'Hello World'
+print(message)
+# but using this approach we have a issue, if i want to print the message with single quote i have to escape >> 'Ali\'s World'. with double quotes we can use single quote
+message = "He said, Ali's World!"
+print(message)
