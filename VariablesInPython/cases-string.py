79394f85102c00ab79cb5398520d44055dd0a8ed
NajeebUrRehmanRaja <najeebraja22.2@gmail.com>
2025-10-06 00:28:24 +0500

    docs(VariablesInPython): add string manipulation examples
    Add examples for count() and replace() string methods to demonstrate additional string manipulation capabilities

--- VariablesInPython/cases-string.py
+++ VariablesInPython/cases-string.py
@@ -5,3 +5,8 @@ print(message.lower())
 print(message.title())
 #  we can find the string by case in reverse order
 print(message.swapcase())
+
+# we can count the number of times a substring appears in a string
+print(message.count("l"))
+#  we can replace the string
+print(message.replace("Python", "Java"))
