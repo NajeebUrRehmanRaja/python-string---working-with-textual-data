32f70513ce415dc373ef21a09a4993d5bbd4cf2d
NajeebUrRehmanRaja <najeebraja22.2@gmail.com>
2025-10-06 00:38:44 +0500

    dir function and help function in python

---
+++ VariablesInPython/dir-function.py
@@ -0,0 +1,6 @@
+#  we can use dir function to find the methods of a string
+message = "Hello Python World"
+print(dir(message))
+
+#  we can use help function to find the documentation of a method
+print(help(message.upper))
